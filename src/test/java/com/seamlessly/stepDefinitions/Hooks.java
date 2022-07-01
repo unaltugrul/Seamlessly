8eb0c3b0c69b522b6064039278eb8361b2a7ca04
unaltugrul <unaltugrulbulut@gmail.com>
2022-07-01 18:10:25 +0300

    Hooks.java

---
+++ src/test/java/com/seamlessly/stepDefinitions/Hooks.java
@@ -0,0 +1,22 @@
+package com.seamlessly.stepDefinitions;
+
+import com.seamlessly.utilities.Driver;
+import io.cucumber.java.After;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+public class Hooks {
+    @After
+    public void teardownScenario(Scenario scenario) {
+
+        if (scenario.isFailed()){
+            byte[] screenshot = ( (TakesScreenshot) Driver.getDriver() ).getScreenshotAs(OutputType.BYTES);
+            scenario.attach(screenshot,"image/png",scenario.getName());
+        }
+
+
+        Driver.closeDriver();
+
+    }
+}
