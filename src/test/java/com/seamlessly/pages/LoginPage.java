0665175acdcfab47fa99da6717ab2b92468b4a56
unaltugrul <unaltugrulbulut@gmail.com>
2022-07-01 18:10:36 +0300

    LoginPage.java

--- src/test/java/com/seamlessly/pages/LoginPage.java
+++ src/test/java/com/seamlessly/pages/LoginPage.java
@@ -1,10 +1,27 @@
 package com.seamlessly.pages;
 
-import com.github.dockerjava.api.model.Driver;
+import com.seamlessly.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class LoginPage {
     public LoginPage(){
         PageFactory.initElements(Driver.getDriver(),this);
     }
+    
+
+    @FindBy(xpath = "//input[@id='user']")
+    public WebElement usernameInputBox;
+
+    @FindBy(xpath = "//input[@id='password']")
+    public WebElement passwordInputBox;
+
+    @FindBy(xpath = "//input[@id='submit-form']")
+    public WebElement logInButton;
+
+    @FindBy(xpath = "//div[@class='logo']")
+    public WebElement loginPagelogo;
+    
+    
 }
